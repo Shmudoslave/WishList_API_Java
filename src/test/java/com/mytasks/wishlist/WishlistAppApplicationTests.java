bd55f20fa3a0baee00b1ef018aba828b65b82363
Shmudoslave <Shmud0808@gmail.com>
2026-03-22 16:11:34 +0300

    First commit

---
+++ src/test/java/com/mytasks/wishlist/WishlistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytasks.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
